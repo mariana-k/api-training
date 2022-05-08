33f24692ad5def2930f33b1737c6d0b455665e81
mariana-k <mariana.kyrkosh@gmail.com>
2022-05-08 16:00:57 +0300

    Updated get requst

--- myApp.js
+++ myApp.js
@@ -1,10 +1,9 @@
 let express = require('express');
 let app = express();
 
-console.log('Hello Express');
-
 app.get('/', (req, res) => {
-    res.send('Hello Express');
+    const absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath);
 });
 
 
